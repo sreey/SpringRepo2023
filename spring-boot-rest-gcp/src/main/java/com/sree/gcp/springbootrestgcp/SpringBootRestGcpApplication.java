4c0b8f05ae7e62562eb60306c08cd2460649bf3b
sreey <sreeyakkanti@gmail.com>
2023-01-22 13:14:42 -0500

    Initial Commit

---
+++ spring-boot-rest-gcp/src/main/java/com/sree/gcp/springbootrestgcp/SpringBootRestGcpApplication.java
@@ -0,0 +1,13 @@
+package com.sree.gcp.springbootrestgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestGcpApplication.class, args);
+	}
+
+}
